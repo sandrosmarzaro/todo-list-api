02d2873face6b1eea85c0072b9c5608ec2120a0b
Sandro Smarzaro <sansmarzaro@gmail.com>
2025-05-31 16:39:46 -0300

    feat: create basic route with route to return html hello world

--- fastapi_zero_course/app.py
+++ fastapi_zero_course/app.py
@@ -1,8 +1,22 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
-app = FastAPI()
+from fastapi_zero_course.schemas import MessageClass
+
+app = FastAPI(title='FastAPI Zero Course API')
 
 
-@app.get('/')
+@app.get('/', status_code=HTTPStatus.OK, response_model=MessageClass)
 def read_root():
     return {'message': 'Hello World!'}
+
+
+@app.get(
+    '/api/v1/hello-world',
+    status_code=HTTPStatus.OK,
+    response_class=HTMLResponse,
+)
+def read_hello_world():
+    return '<html><body><h1>Hello World!</h1></body></html>'
--- tests/test_app.py
+++ tests/test_app.py
@@ -12,3 +12,12 @@ def test_root_should_return_hello_world():
 
     assert response.json() == {'message': 'Hello World!'}
     assert response.status_code == HTTPStatus.OK
+
+
+def test_api_v1_should_return_html_hello_world():
+    client = TestClient(app)
+
+    response = client.get('/api/v1/hello-world')
+
+    assert response.text == '<html><body><h1>Hello World!</h1></body></html>'
+    assert response.status_code == HTTPStatus.OK
