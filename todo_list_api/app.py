39ed64d326850a516e2104360ebfc0581f86e6f3
Sandro Smarzaro <sansmarzaro@gmail.com>
2025-06-06 19:45:11 -0300

    feat: intial configuration of orm in post user

--- todo_list_api/app.py
+++ todo_list_api/app.py
@@ -34,9 +34,39 @@ def read_hello_world():
     response_model=UserPublic,
 )
 def create_user(user: UserSchema):
-    user_with_id = UserPublic(**user.model_dump(), id=len(database) + 1)
-    database.append(user_with_id)
-    return user_with_id
+    from sqlalchemy import create_engine, select
+    from todo_list_api.settings import Settings
+    from sqlalchemy.orm import Session
+    from todo_list_api.models import User
+
+    engine = create_engine(Settings.DATABASE_URL)
+    session = Session(engine)
+    if db_user := session.scalar(
+        select(User).where(
+            User.username == user.username | User.email == user.email
+        )
+    ):
+        if db_user.username == user.username:
+            raise HTTPException(
+                detail='Username already exists!',
+                status_code=HTTPStatus.CONFLICT,
+            )
+        elif db_user.email == user.email:
+            raise HTTPException(
+                detail='Email already exists!',
+                status_code=HTTPStatus.CONFLICT,
+            )
+
+        db_user = User(
+            username=db_user.username,
+            password=db_user.password,
+            email=db_user.email,
+        )
+        session.add(db_user)
+        session.commit()
+        session.refresh(db_user)
+
+        return db_user
 
 
 @app.get(
