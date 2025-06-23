4c51dad886cc2ed3c06b27597ce93c7193db26f6
Sandro Smarzaro <sansmarzaro@gmail.com>
2025-06-23 20:41:06 -0300

    test: create token fixtures and update unit with authorization

--- tests/test_app.py
+++ tests/test_app.py
@@ -39,9 +39,9 @@ def test_api_v1_users_post_should_raise_username_exception(client, user):
     response = client.post(
         '/api/v1/users',
         json={
-            'username': 'test',
-            'email': 'email_2nd@example.com',
-            'password': 'test123',
+            'username': user.username,
+            'email': user.email,
+            'password': user.password,
         },
     )
 
@@ -63,24 +63,21 @@ def test_api_v1_users_post_should_raise_email_exception(client, user):
     assert response.json() == {'detail': 'Email already exists!'}
 
 
-def test_api_v1_users_get_should_return_empty_users(client):
-    response = client.get('/api/v1/users')
-
-    assert response.status_code == HTTPStatus.OK
-    assert response.json() == {'users': []}
-
-
-def test_api_v1_users_get_should_return_one_in_users(client, user):
+def test_api_v1_users_get_should_return_one_in_users(client, user, token):
     user_schema = UserPublic.model_validate(user).model_dump()
-    response = client.get('/api/v1/users')
+    response = client.get(
+        '/api/v1/users',
+        headers={'Authorization': f'Bearer {token}'},
+    )
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'users': [user_schema]}
 
 
-def test_api_v1_users_put_should_update_user(client, user):
+def test_api_v1_users_put_should_update_user(client, user, token):
     response = client.put(
         '/api/v1/users/1',
+        headers={'Authorization': f'Bearer {token}'},
         json={
             'username': 'name_updated',
             'email': 'newemail@example.com',
@@ -96,20 +93,10 @@ def test_api_v1_users_put_should_update_user(client, user):
     }
 
 
-def test_api_v1_users_put_should_raise_exception(client):
-    response = client.put(
-        '/api/v1/users/-1',
-        json={
-            'username': 'name_updated',
-            'email': 'newemail@example.com',
-            'password': 'uPd4t$d',
-        },
-    )
-
-    assert response.status_code == HTTPStatus.NOT_FOUND
-
+def test_api_v1_users_put_should_raise_exception(client, user, token):
     response = client.put(
         '/api/v1/users/2',
+        headers={'Authorization': f'Bearer {token}'},
         json={
             'username': 'name_updated',
             'email': 'newemail@example.com',
@@ -117,11 +104,11 @@ def test_api_v1_users_put_should_raise_exception(client):
         },
     )
 
-    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.status_code == HTTPStatus.FORBIDDEN
 
 
 def test_api_v1_users_put_should_raise_exeception_when_is_not_unique(
-    client, user
+    client, user, token
 ):
     client.post(
         '/api/v1/users',
@@ -133,6 +120,7 @@ def test_api_v1_users_put_should_raise_exeception_when_is_not_unique(
     )
     response = client.put(
         f'/api/v1/users/{user.id}',
+        headers={'Authorization': f'Bearer {token}'},
         json={
             'username': 'test2nd',
             'email': 'second@email.com',
@@ -144,42 +132,46 @@ def test_api_v1_users_put_should_raise_exeception_when_is_not_unique(
     assert response.json() == {'detail': 'Username or email already exists!'}
 
 
-def test_api_v1_users_get_should_return_user(client, user):
-    response = client.get(f'/api/v1/users/{user.id}')
+def test_api_v1_users_get_should_return_user(client, user, token):
+    response = client.get(
+        f'/api/v1/users/{user.id}',
+        headers={'Authorization': f'Bearer {token}'},
+    )
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {
-        'id': 1,
-        'username': 'test',
-        'email': 'email@example.com',
+        'id': user.id,
+        'username': user.username,
+        'email': user.email,
     }
 
 
-def test_api_v1_users_get_should_raise_exception(client):
-    response = client.get('/api/v1/users/-1')
+def test_api_v1_users_get_should_raise_exception(client, user, token):
+    response = client.get(
+        f'/api/v1/users/{user.id + 1}',
+        headers={'Authorization': f'Bearer {token}'},
+    )
 
-    assert response.status_code == HTTPStatus.NOT_FOUND
-
-    response = client.get('/api/v1/users/2')
-
-    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.status_code == HTTPStatus.FORBIDDEN
 
 
-def test_api_v1_users_delete_should_remove_user(client, user):
-    response = client.delete('/api/v1/users/1')
+def test_api_v1_users_delete_should_remove_user(client, user, token):
+    response = client.delete(
+        f'/api/v1/users/{user.id}',
+        headers={'Authorization': f'Bearer {token}'},
+    )
 
     assert response.status_code == HTTPStatus.NO_CONTENT
     assert response.content == b''
 
 
-def test_api_v1_users_delete_should_raise_exception(client):
-    response = client.delete('/api/v1/users/-1')
+def test_api_v1_users_delete_should_raise_exception(client, user, token):
+    response = client.delete(
+        f'/api/v1/users/{user.id + 1}',
+        headers={'Authorization': f'Bearer {token}'},
+    )
 
-    assert response.status_code == HTTPStatus.NOT_FOUND
-
-    response = client.delete('/api/v1/users/2')
-
-    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.status_code == HTTPStatus.FORBIDDEN
 
 
 def test_api_v1_token_create_token(client, user):
@@ -193,3 +185,23 @@ def test_api_v1_token_create_token(client, user):
     assert response.status_code == HTTPStatus.OK
     assert 'access_token' in token
     assert token['token_type'] == 'Bearer'
+
+
+def test_api_v1_token_raise_exception_when_invalid_email(client):
+    response = client.post(
+        '/api/v1/token',
+        data={'username': 'invalid@email.com', 'password': 'test123'},
+    )
+
+    response.status_code == HTTPStatus.UNAUTHORIZED
+    response.json() == {'detail': 'Email doesnt exists!'}
+
+
+def test_api_v1_token_raise_exception_when_invalid_password(client, user):
+    response = client.post(
+        '/api/v1/token',
+        data={'username': user.email, 'password': 'invalid_password'},
+    )
+
+    response.status_code == HTTPStatus.UNAUTHORIZED
+    response.json() == {'detail': 'Incorrect password!'}
--- tests/test_security.py
+++ tests/test_security.py
@@ -1,6 +1,19 @@
-from jwt import decode
+from datetime import datetime, timedelta
+from http import HTTPStatus
+from zoneinfo import ZoneInfo
 
-from todo_list_api.security import ALGORITHM, SECRET_KEY, create_access_token
+from jwt import decode, encode
+
+from todo_list_api.security import (
+    ACCESS_TOKEN_EXPIRE_MINUTES,
+    ALGORITHM,
+    SECRET_KEY,
+    create_access_token,
+)
+
+EXPIRE = datetime.now(tz=ZoneInfo('UTC')) + timedelta(
+    minutes=ACCESS_TOKEN_EXPIRE_MINUTES
+)
 
 
 def test_create_jwt():
@@ -10,3 +23,51 @@ def test_create_jwt():
 
     assert decoded['test'] == claims['test']
     assert 'exp' in decoded
+
+
+def test_jwt_invalid(client):
+    response = client.delete(
+        '/api/v1/users/1',
+        headers={'Authorization': 'Bearer invalid_token'},
+    )
+
+    assert response.status_code == HTTPStatus.UNAUTHORIZED
+    assert response.json() == {'detail': 'Could not validate credentials'}
+
+
+def test_invalid_claims_without_sub(client, user):
+    invalid_token = encode(
+        {
+            'sub': '',
+            'exp': EXPIRE,
+        },
+        SECRET_KEY,
+        algorithm=ALGORITHM,
+    )
+
+    response = client.get(
+        f'/api/v1/users/{user.id}',
+        headers={'Authorization': f'Bearer {invalid_token}'},
+    )
+
+    assert response.status_code == HTTPStatus.UNAUTHORIZED
+    assert response.json() == {'detail': 'Could not validate credentials'}
+
+
+def test_invalid_email_doesnt_exists(client, user):
+    token = encode(
+        {
+            'sub': 'invalid@email.com',
+            'exp': EXPIRE,
+        },
+        SECRET_KEY,
+        algorithm=ALGORITHM,
+    )
+
+    response = client.get(
+        f'/api/v1/users/{user.id}',
+        headers={'Authorization': f'Bearer {token}'},
+    )
+
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {'detail': 'User doesnt exists'}
