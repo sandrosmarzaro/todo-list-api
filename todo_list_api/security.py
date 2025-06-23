be695d6a4480ace29fb76d0e61ce9bab60a0b079
Sandro Smarzaro <sansmarzaro@gmail.com>
2025-06-23 16:32:20 -0300

    feat: add method to create jwt

--- todo_list_api/security.py
+++ todo_list_api/security.py
@@ -1,6 +1,20 @@
+from datetime import datetime, timedelta
+from zoneinfo import ZoneInfo
+
+from fastapi import Depends
+from fastapi.security import OAuth2AuthorizationCodeBearer
+from jwt import encode
 from pwdlib import PasswordHash
+from sqlalchemy.orm import Session
+
+from todo_list_api.database import get_session
 
 pwd_context = PasswordHash.recommended()
+oauth2_scheme = OAuth2AuthorizationCodeBearer(tokenUrl='/api/v1/token')
+
+SECRET_KEY = ''
+ALGORITHM = 'HS256'
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 
 def get_password_hash(password: str):
@@ -9,3 +23,14 @@ def get_password_hash(password: str):
 
 def verify_password(plain_password: str, hashed_password: str):
     return pwd_context.verify(plain_password, hashed_password)
+
+
+def create_access_token(data: dict):
+    claims = data.copy()
+
+    expire = datetime.now(tz=ZoneInfo('UTC')) + timedelta(
+        minutes=ACCESS_TOKEN_EXPIRE_MINUTES
+    )
+    claims['exp'] = expire
+
+    return encode(claims, SECRET_KEY, algorithm=ALGORITHM)
