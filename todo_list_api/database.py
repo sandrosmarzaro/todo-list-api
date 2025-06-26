7f59302bf8be3a16533cbf77d34e8770417383bb
Sandro Smarzaro <sansmarzaro@gmail.com>
2025-06-26 19:53:40 -0300

    test: dont cover database ssesion creation

--- todo_list_api/database.py
+++ todo_list_api/database.py
@@ -5,6 +5,6 @@ from todo_list_api.settings import Settings
 engine = create_async_engine(Settings().DATABASE_URL)
 
 
-async def get_session():
+async def get_session():  # pragma: no cover
     async with AsyncSession(engine, expire_on_commit=False) as session:
         yield session
