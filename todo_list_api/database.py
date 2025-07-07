50cf6e2084e8a36b0d5b8cf99f5a85f145e30ee0
Sandro Smarzaro <sansmarzaro@gmail.com>
2025-07-07 12:23:17 -0300

    build: deploy in fly.io

--- todo_list_api/database.py
+++ todo_list_api/database.py
@@ -2,7 +2,12 @@ from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 
 from todo_list_api.settings import Settings
 
-engine = create_async_engine(Settings().DATABASE_URL)
+engine = create_async_engine(
+    Settings().DATABASE_URL,
+    max_overflow=10,
+    pool_size=5,
+    pool_recycle=200,
+)
 
 
 async def get_session():  # pragma: no cover
